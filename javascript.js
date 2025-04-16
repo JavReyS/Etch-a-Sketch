0afadfe9d82912286d75d4b0404bf907527e8e80
JavReyS <luisjavier-bdq5@live.com.mx>
2025-04-15 21:33:11 -0600

    Add function to paint with random colors
    Added buttons to change paint mode (black o color)
    Added function to change color mode
    Added function to generate a random color
    Added functions to hide and display paint mode available

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let currentMode = "black";
+const blackBtn = document.querySelector('.black');
+const randomBtn = document.querySelector('.random');
+blackBtn.style.display = 'none';
 
 const createGrid = (size) => {
     const container = document.querySelector('.gridSquareDiv');
@@ -9,7 +13,7 @@ const createGrid = (size) => {
         square.setAttribute("style", "box-sizing: border-box; width:" +squareSize+ 
             "px; height:" +squareSize+ "px; border: 1px solid #eee");
         square.addEventListener('mouseenter', function () {
-            square.style.backgroundColor = "black";
+            square.style.backgroundColor = getColor(currentMode);
         })
         container.appendChild(square);
     }
@@ -18,16 +22,51 @@ const createGrid = (size) => {
 const resize = () => {
     const resizeBtn = document.querySelector('.resize');
     resizeBtn.addEventListener('click', () => {
-    let newSize = prompt('Enter new grid size (max 100):');
-    newSize = parseInt(newSize);
-    if (isNaN(newSize) || newSize < 1 || newSize > 100) {
-      alert('Please enter a number between 1 and 100.');
-      return;
+        let newSize = prompt('Enter new grid size (max 100):');
+        newSize = parseInt(newSize);
+        if (isNaN(newSize) || newSize < 1 || newSize > 100) {
+            alert('Please enter a number between 1 and 100.');
+            return;
+        }
+        createGrid(newSize);
+    });
+}
+
+const randomColor = () => {
+    randomBtn.addEventListener('click', () => {
+        currentMode = "random";
+        randomBtn.style.display = 'none';
+        blackBtn.style.display = 'block';
+    });
+}
+
+const blackColor = () => {
+    blackBtn.addEventListener('click', () => {
+        currentMode = "black";
+        blackBtn.style.display = 'none';
+        randomBtn.style.display = 'block';
+    });
+}
+
+function getColor(currentMode) {
+    if(currentMode == "black")
+        color = "black";
+    else if(currentMode == "random")
+        color = generateRandomColor();
+    return color;
+}
+
+function generateRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
-    createGrid(newSize);
-  });
+    return color;
 }
 
 
 createGrid(16);
 resize();
+randomColor();
+blackColor();
