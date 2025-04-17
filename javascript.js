f92c8db24fcaaba5e55b23d9839de62c3a5f1379
JavReyS <luisjavier-bdq5@live.com.mx>
2025-04-16 23:43:18 -0600

    Add function to archieve a darkening effect
    Added button to darkening colors
    Added opacity atribute and set it to 0 for each square
    Added function to get actual square opacity value
    Added function to change to on/off darkening effect

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 let currentMode = "black";
 const blackBtn = document.querySelector('.black');
 const randomBtn = document.querySelector('.random');
+let darkening = "off";
 blackBtn.style.display = 'none';
 
 const createGrid = (size) => {
@@ -12,7 +13,9 @@ const createGrid = (size) => {
         square.classList.add("square");
         square.setAttribute("style", "box-sizing: border-box; width:" +squareSize+ 
             "px; height:" +squareSize+ "px; border: 1px solid #eee");
+        square.currentOPacity = 0;
         square.addEventListener('mouseenter', function () {
+            square.style.opacity = getOpacity(darkening, square);
             square.style.backgroundColor = getColor(currentMode);
         })
         container.appendChild(square);
@@ -48,12 +51,39 @@ const blackColor = () => {
     });
 }
 
+const darkeningEffect = () => {
+    const darkeningBtn = document.querySelector('.darkening');
+    darkeningBtn.addEventListener('click', () => {
+        if(darkening == "on") {
+            darkening = "off";
+            darkeningBtn.textContent = "On darkening";
+        } else {
+            darkening = "on";
+            darkeningBtn.textContent = "Off darkening";
+        }
+    })
+}
+
 function getColor(currentMode) {
     if(currentMode == "black")
         color = "black";
     else if(currentMode == "random")
         color = generateRandomColor();
     return color;
+}
+
+function getOpacity(darkening, square){
+    if(square.currentOPacity >= 1) { //reset current opacity
+        square.currentOPacity = 0;
+    }
+    if(darkening == "on") {
+        square.currentOPacity += 0.1;
+        opacity = square.currentOPacity;
+    } else {
+        square.currentOPacity = 1;
+        opacity = 1;
+    }
+    return opacity;
 }
 
 function generateRandomColor() {
@@ -65,8 +95,8 @@ function generateRandomColor() {
     return color;
 }
 
-
 createGrid(16);
 resize();
 randomColor();
 blackColor();
+darkeningEffect();
